3464429b08b75fb643cb557cace6e93e73b4a3ab
nvnrdhn <novanardhn_@live.com>
2021-06-18 17:45:47 +0700

    details activity

---
+++ app/src/main/java/com/nvnrdhn/bajps3/ui/details/DetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.nvnrdhn.bajps3.ui.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nvnrdhn.bajps3.R
+
+class DetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details)
+    }
+}
